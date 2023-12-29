d3bd9c20104c31f3524678b8929f0e6327d98716
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:31:04 +0530

    Header Component - styled

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 const Header = () => {
-	return <div>Header</div>
+	return (
+		<header className='flex h-20 w-full items-center justify-center px-6 bg-stone-300'>
+			<h1 className='text-3xl font-todo font-bold text-stone-600'>
+				Todo Application
+			</h1>
+		</header>
+	)
 }
 
 export default Header
