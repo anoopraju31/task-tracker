a7d4424feee2aed3e159e769ae926a0d94c221e9
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:20:57 +0530

    Header Component - initial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
+import Header from '../components/Header'
+
 const Home = () => {
-	return <div className='container mx-auto max-w-3xl'> Home Page </div>
+	return (
+		<div className='container mx-auto max-w-3xl'>
+			<Header />
+		</div>
+	)
 }
 
 export default Home
