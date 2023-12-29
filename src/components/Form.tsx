22db2de01ced29d6d71068fe526ad59fae4be2a2
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:40:41 +0530

    Input Field Component - initial

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,11 @@
+import InputField from './InputField'
+
 const Form = () => {
-	return <div>Form</div>
+	return (
+		<form className='p-6 flex flex-col gap-2'>
+			<InputField />
+		</form>
+	)
 }
 
 export default Form
