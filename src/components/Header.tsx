2995d235ac65ffcd4a92b7b2acca3193f490afda
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 21:04:14 +0530

    Header Component - reactor

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
 	return (
 		<header className='flex h-20 w-full items-center justify-center px-6 bg-stone-300'>
 			<h1 className='text-3xl font-todo font-bold text-stone-600'>
-				Todo Application
+				Tasks Tracker
 			</h1>
 		</header>
 	)
