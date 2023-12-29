7687f3b3bd507986a5301ac10d5d870b5c17c469
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:49:50 +0530

    Input Field Component - styled

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,30 @@
+import { ChangeEvent } from 'react'
 import InputField from './InputField'
 
 const Form = () => {
 	return (
-		<form className='p-6 flex flex-col gap-2'>
-			<InputField />
+		<form className='p-6 flex flex-col gap-4'>
+			<InputField
+				id='title'
+				label='Title'
+				placeholder='Enter title'
+				type='text'
+				value={''}
+				handleChange={(e: ChangeEvent<HTMLInputElement>) => {
+					console.log(e)
+				}}
+			/>
+
+			<InputField
+				id='description'
+				label='Description'
+				placeholder='Enter description'
+				type='text'
+				value={''}
+				handleChange={(e: ChangeEvent<HTMLInputElement>) => {
+					console.log(e)
+				}}
+			/>
 		</form>
 	)
 }
