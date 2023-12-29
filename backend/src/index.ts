bea75be42d94196cf9888335356d1afc0d9f1f57
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 22:16:35 +0530

    Express server created

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello World')
+import express from 'express'
+
+const app = express()
+const PORT = 5500
+
+app.get('/', (req, res) => {
+	res.json({ message: 'Welcome to Task tracker server.' })
+})
+
+app.listen(PORT, () => {
+	console.log(`Server started at port ${PORT}.`)
+})
