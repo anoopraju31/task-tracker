62490c9e301c2fb2092e9b198b7fea693b652be6
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:35:29 +0530

    Form Component - initial

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,5 +1,17 @@
+import Form from './Form'
+
 const TodoForm = () => {
-	return <div>TodoForm</div>
+	return (
+		<section className='my-10 rounded-lg border border-stone-600 text-stone-500 shadow-sm'>
+			<div className='flex flex-col space-y-1.5 p-6'>
+				<h3 className='font-semibold text-center font-todo tracking-tight text-lg'>
+					Add New Task
+				</h3>
+			</div>
+
+			<Form />
+		</section>
+	)
 }
 
 export default TodoForm
