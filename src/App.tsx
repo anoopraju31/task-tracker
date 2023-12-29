f974775b3139e2ad312bfa1e5afc4edc2b97c2ce
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:19:53 +0530

    Home Page - added to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Home from './pages/Home'
+
 function App() {
 	return (
-		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
-		</>
+		<main className='p-10 bg-stone-300 min-h-screen'>
+			<Home />
+		</main>
 	)
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-	return <div>Home</div>
+	return <div className='container mx-auto max-w-3xl'> Home Page </div>
 }
 
 export default Home
