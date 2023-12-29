43dd7d7fc0134c5fc4cc5bb4323517e11f2926b3
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 21:48:16 +0530

    Footer component - styled

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,9 @@
 const Footer = () => {
-	return <div>Footer</div>
+	return (
+		<footer className='mt-10 p-5 rounded-lg flex justify-center items-center bg-stone-200 '>
+			<p className='font-todo text-stone-600 font-extralight'> Made with❤️ </p>
+		</footer>
+	)
 }
 
 export default Footer
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from '../components/Header'
 import TasksContainer from '../components/TasksContainer'
 import TaskForm from '../components/TaskForm'
+import Footer from '../components/Footer'
 
 const Home = () => {
 	return (
@@ -8,6 +9,7 @@ const Home = () => {
 			<Header />
 			<TaskForm />
 			<TasksContainer />
+			<Footer />
 		</div>
 	)
 }
