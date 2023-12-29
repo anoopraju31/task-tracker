d77b19e8ea8390b56ef8b8b8933164e7d672e0da
anoopraju31 <anoopraju31@gmail.com>
2023-12-29 20:32:19 +0530

    Todo Form Component - initial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header'
+import TodoForm from '../components/TodoForm'
 
 const Home = () => {
 	return (
 		<div className='container mx-auto max-w-3xl'>
 			<Header />
+			<TodoForm />
 		</div>
 	)
 }
