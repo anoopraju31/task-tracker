07a04f2883407564fe2853d722edadc23a6aff71
anoopraju31 <anoopraju31@gmail.com>
2023-12-30 16:04:03 +0530

    Task validation added

--- backend/src/utills/validations.ts
+++ backend/src/utills/validations.ts
@@ -1,15 +1,13 @@
 import z from 'zod'
 
-const nameSchema = z.string().min(1, { message: 'Name is required' })
-
+const nameSchema = z.string().min(1, { message: 'Name is required.' })
 const emailSchema = z
 	.string()
 	.min(1, { message: 'Email is required.' })
 	.email('Invalid email.')
-
 const passwordSchema = z
 	.string()
-	.min(8, { message: 'Password must contain atleast 8 characters' })
+	.min(8, { message: 'Password must contain atleast 8 characters.' })
 
 export const signUpSchema = z.object({
 	name: nameSchema,
@@ -20,4 +18,25 @@ export const signUpSchema = z.object({
 export const signInSchema = z.object({
 	email: emailSchema,
 	password: passwordSchema,
+})
+
+const titleSchema = z.string().min(1, { message: 'Title is required.' })
+const descriptionSchema = z.string()
+const targetDateSchema = z
+	.string()
+	.datetime({ message: 'Invalid target date.' })
+	.refine(
+		(str) => {
+			const date = new Date(str)
+			return !isNaN(date.getTime()) && date.getTime() > Date.now()
+		},
+		{
+			message: 'target date must be a valid future date.',
+		},
+	)
+
+export const taskSchema = z.object({
+	title: titleSchema,
+	description: descriptionSchema,
+	targetDate: targetDateSchema,
 })
