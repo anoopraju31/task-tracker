051b089b5b51e85a97ef656b13fb944a469d66f0
anoopraju31 <anoopraju31@gmail.com>
2023-12-30 15:24:13 +0530

    JWT - verify token

--- backend/src/utills/jwt.ts
+++ backend/src/utills/jwt.ts
@@ -1,9 +1,26 @@
 import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
+type Payload = {
+	name: string
+	email: string
+	id: string
+}
+
 const secret = process.env.JWT_SECRET as string
 
-export const generateToken = (payload: object) => {
+export const generateToken = (payload: Payload) => {
 	const token = jwt.sign(payload, secret, { expiresIn: '30d' })
 	return token
+}
+
+export const verifyToken = (token: string): string | null => {
+	try {
+		const decoded: Payload = jwt.verify(token, secret) as Payload
+
+		return decoded.id
+	} catch (error) {
+		console.error(error)
+		return null
+	}
 }
