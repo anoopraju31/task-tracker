8f836a41b7930b0c71d289cb4a3abd1ec29cea0a
anoopraju31 <anoopraju31@gmail.com>
2023-12-30 13:42:36 +0530

    User route - sign up added

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,37 @@
 import express from 'express'
+import { signUpSchema } from '../utills/validations'
+import { createUser, getUserByEmail } from '../models/user.model'
+import { encryptPassword } from '../utills/password'
 
 const router = express.Router()
+
+router.post('/sign-up', async (req, res) => {
+	try {
+		const { name, email, password } = req.body
+		const validationResponse = signUpSchema.safeParse({ name, email, password })
+
+		if (!validationResponse.success) {
+			console.log(validationResponse)
+			res.status(401).json(validationResponse.error.issues[0].message)
+		}
+
+		const isUserExists = await getUserByEmail(email)
+
+		if (isUserExists)
+			return res.status(400).json({ message: 'User already exists.' })
+
+		const hashedPassword = await encryptPassword(password)
+
+		await createUser({
+			name,
+			email,
+			authentication: { password: hashedPassword },
+		})
+
+		res.json({ message: 'User created successfully.' })
+	} catch (error) {
+		console.error(error)
+	}
+})
 
 export default router
