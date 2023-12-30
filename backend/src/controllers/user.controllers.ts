6b135eed4c5d7326850e88e772e8d45de80d8693
anoopraju31 <anoopraju31@gmail.com>
2023-12-30 14:52:59 +0530

    User Controller - sign in controller added

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,7 +1,12 @@
 import express from 'express'
-import { signUpSchema } from '../utills/validations'
-import { createUser, getUserByEmail } from '../models/user.model'
-import { encryptPassword } from '../utills/password'
+import { signInSchema, signUpSchema } from '../utills/validations'
+import {
+	createUser,
+	getFullUserDetailsByEmail,
+	getUserByEmail,
+} from '../models/user.model'
+import { comparePassword, encryptPassword } from '../utills/password'
+import { generateToken } from '../utills/jwt'
 
 export const signUpController = async (
 	req: express.Request,
@@ -31,4 +36,31 @@ export const signUpController = async (
 	} catch (error) {
 		console.error(error)
 	}
+}
+
+export const signInController = async (
+	req: express.Request,
+	res: express.Response,
+) => {
+	const { email, password } = req.body
+	const validationResponse = signInSchema.safeParse({ email, password })
+
+	if (!validationResponse.success)
+		return res.status(401).json(validationResponse.error.issues[0].message)
+
+	const user = await getFullUserDetailsByEmail(email)
+
+	if (!user) return res.status(401).json({ message: 'User does not exists.' })
+
+	const passwordMatch = await comparePassword(
+		password,
+		user.authentication?.password!,
+	)
+
+	if (!passwordMatch)
+		return res.status(403).json({ message: 'Invalid password' })
+
+	const token = generateToken({ name: user.name, email, id: user._id })
+
+	res.json({ message: 'successfully signed in.', token })
 }
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,36 +1,12 @@
 import express from 'express'
-import { signInSchema } from '../utills/validations'
-import { getFullUserDetailsByEmail } from '../models/user.model'
-import { comparePassword } from '../utills/password'
-import { generateToken } from '../utills/jwt'
-import { signUpController } from '../controllers/user.controllers'
+import {
+	signInController,
+	signUpController,
+} from '../controllers/user.controllers'
 
 const router = express.Router()
 
 router.post('/sign-up', signUpController)
-
-router.post('/sign-in', async (req, res) => {
-	const { email, password } = req.body
-	const validationResponse = signInSchema.safeParse({ email, password })
-
-	if (!validationResponse.success)
-		return res.status(401).json(validationResponse.error.issues[0].message)
-
-	const user = await getFullUserDetailsByEmail(email)
-
-	if (!user) return res.status(401).json({ message: 'User does not exists.' })
-
-	const passwordMatch = await comparePassword(
-		password,
-		user.authentication?.password!,
-	)
-
-	if (!passwordMatch)
-		return res.status(403).json({ message: 'Invalid password' })
-
-	const token = generateToken({ name: user.name, email, id: user._id })
-
-	res.json({ message: 'successfully signed in.', token })
-})
+router.post('/sign-in', signInController)
 
 export default router
